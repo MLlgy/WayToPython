03e4d460beb653254016310d97b037a422be0939
liguoying <alinemklee@yeah.net>
2017-11-07 18:21:59 +0800

    filter .py

--- one/filter.py
+++ one/filter.py
@@ -3,3 +3,14 @@
 def is_odd(n):
     return n % 2 == 1
 print(list(filter(is_odd,[3,56,7,8,9])))
+
+def susu(n):
+    if n <= 2:
+      return True
+    for i in range(2,n):
+        if n % i == 0:
+          return False
+        else:
+            return True
+print(list(filter(susu,range(1,101))))
+print(list(range(1,101)))
