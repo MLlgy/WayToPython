2c6cc46cf1722936b79f0b0a5dfee563d0359dd2
liguoying <alinemklee@yeah.net>
2017-12-13 14:16:01 +0800

    mkdir file

--- LearnWithVideo/lesson29_read_file.py
+++ LearnWithVideo/lesson29_read_file.py
@@ -1,6 +1,6 @@
 def read_file():
     try:
-        f = open('/home/mk.io/下载/python视频/WayToPython/LearnWithVideo/fileone', 'r')
+        f = open('./file/fileone', 'r')
         print(f.readlines())
     except BaseException as e:
         print(e)
@@ -8,16 +8,20 @@ def read_file():
         if f:
             f.close()
 
+
 read_file()
+
+
 #
 def read_byte_file():
     try:
-        f = open('/home/mk.io/图片/235.png','r',encoding='gbk',errors='ignore')
+        f = open('./file/235.png', 'r', encoding='gbk', errors='ignore')
         print(f.read())
     except BaseException as e:
         print(e)
     finally:
         if f:
             f.close()
+
 
 read_byte_file()
